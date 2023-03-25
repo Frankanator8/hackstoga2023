ab48bd0fa8987d9f0ac48fa32ba1f5a81f67b67e
Raphie <100081255+r4Ph13@users.noreply.github.com>
2023-03-25 10:50:18 -0400

    ahhhh

--- GUI.py
+++ GUI.py
@@ -3,6 +3,7 @@ import loader
 loadingImage = loader.loadImage("loadingImage.py")
 startingImage = loader.loadImage("startingImage.py")
 def draw(screen, mode):
+    font = pygame.font.SysFont('Times new Roman', 30)
     buttons = []
     if(mode=='loading'):
         screen.blit(loadingImage, (500,500))
